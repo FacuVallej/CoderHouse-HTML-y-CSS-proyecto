415691acd54826dc1628df82825a49162b59402f
Facundo <facuvallejo00@gmail.com>
2023-01-04 23:30:00 -0300

    se agrega javascript al proyecto

---
+++ JAVA/main.js
@@ -0,0 +1,7 @@
+let nombreIngresado = prompt("¿Cual es su nombre");
+if (nombreIngresado == "") {
+    alert("Por favor, ingrese su nombre")
+}
+else {
+    alert("Bienvenida a Club de la birra " + nombreIngresado)
+}
